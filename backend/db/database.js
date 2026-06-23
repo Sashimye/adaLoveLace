e2a766ba7c667d5b1ac42f164dd21aa655149a65
Justin <justinbuzze@gmail.com>
2026-06-23 15:42:39 +0200

    creation of table

--- backend/db/database.js
+++ backend/db/database.js
@@ -2,30 +2,56 @@ const sqlite3 = require("sqlite3");
 const path = require("path");
 const { open } = require("sqlite");
 
+// Ouvre (ou crée) le fichier de base de données
+
 async function getDB() {
-  const db = await open({
+  return open({
     filename: path.join(__dirname, "adalovelace.db"),
     driver: sqlite3.Database,
   });
-  return db;
 }
+
+// Crée toutes les tables si elles n'existent pas encore
 
 async function initDB() {
   const db = await getDB();
 
   await db.exec(`
-        CREATE TABLE IF NOT EXISTS users(
-        id          TEXT NOT NULL PRIMARY KEY,
-        nom         TEXT NOT NULL 
-        )
-    `);
+    CREATE TABLE IF NOT EXIST users (
+    id          TEXT PRIMARY KEY,
+    email       TEXT NOT NULL UNIQUE, 
+    password    TEXT NOT NULL, 
+    name        TEXT NOT NULL
+    );
 
-  await db.exec(`
-      INSERT INTO users VALUES
-      ('1','Justin')
-`);
-  console.log("Database ready");
+    CREATE TABLE IF NOT EXIST projects (
+    id          TEXT PRIMARY KEY,
+    title       TEXT NOT NULL,
+    description TEXT,
+    status      TEXT DEFAULT 'planning',
+    owner_id    TEXT NOT NULL,
+    start_date  TEXT,
+    deadline    TEXT,
+    created_at  TEXT DEFAULT (datetime('now'))
+  );
+
+
+    CREATE TABLE IF NOT EXIST tasks (
+    id          TEXT PRIMARY KEY,
+    title       TEXT NOT NULl,
+    status      TEXT DEFAULT 'todo',
+    project_id  TEXT NOT NULL,
+    assigned_to TEXT,
+    created_at  TEXT DEFAULT (datetime('now'))
+    );
+
+    CREATE TABLE IF NOT EXISTS project_members (
+    project_id TEXT NOT NULL,
+    user_id    TEXT NOT NULL,
+    PRIMARY KEY (project_id, user_id)
+    );
+    `);
+  console.log('Base de données prête')
 }
 
-initDB();
-module.exports = { getDB, initDB };
+module.exports = { getDB, initDB }
